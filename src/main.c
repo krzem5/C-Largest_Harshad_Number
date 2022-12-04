1a816a7a9faa322c82f7ac1539c7e28c6bd246aa
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-12-04 10:12:39 +0100

    Push Update 04/12/2022, 10:12:39

--- src/main.c
+++ src/main.c
@@ -6,8 +6,8 @@
 
 #define BOUND_LOWER 0xffffffff11ul
 #define BOUND_UPPER 0xfffffffffful
-	
-	
+
+
 
 int main(int argc,const char** argv){
 	printf("Largest harshard number between %lx and %lx: %lx\n",BOUND_LOWER,BOUND_UPPER,largest_harshard_number(BOUND_LOWER,BOUND_UPPER));
